3703607c39833e94a4d35d50967be4f4fead1b03
Brijesh Panchal <brijeshpanchal127@gmail.com>
2021-01-04 23:26:35 -0500

    working on wishlist

--- src/components/ProductList/productList.component.js
+++ src/components/ProductList/productList.component.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import Product from '../Products/products.component';
 import './productlist.style.css';
-
+import {updateCartWithWishListData} from '../../actions/action';
 import axios from 'axios'
 
 
@@ -10,8 +10,23 @@ import axios from 'axios'
 const ProductList = () => {
   const [productData, setProductData] = useState([]);
   const items = useSelector(state => state.selectedDataForCart);
+  const dispatch = useDispatch();
 
   useEffect(() => {
+
+    axios.get("http://localhost:3000/wishlist")
+    .then(function(response){
+      console.log("WishList",response.data.data);
+      response.data.data.map(item=>{
+        console.log("item", item);
+        dispatch(updateCartWithWishListData(item));
+      })
+    })
+    .catch(function(error){
+      console.log(error);
+    })
+
+
     axios.get("http://localhost:3000/")
     .then(function (response) {
       console.log(response);
